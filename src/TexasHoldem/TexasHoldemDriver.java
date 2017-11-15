9afc4d91cf75270eaab9b48c2ea5ec654a902a75
Mark O Donoghue <t00196848@365s.ittralee.ie>
2017-11-15 16:29:34 +0000

    Initial commit

---
+++ src/TexasHoldem/TexasHoldemDriver.java
@@ -0,0 +1,29 @@
+package TexasHoldem;
+
+import javax.swing.*;
+
+public class TexasHoldemDriver {
+    public static void main(String[] args) {
+        int num_Players;
+        String name;
+        double balance;
+
+        num_Players = Integer.parseInt(JOptionPane.showInputDialog("Please enter how many players will be taking part"));
+
+        Player[] players = new Player[num_Players];
+
+
+        for (int i = 0; i < num_Players; i++) {
+            name = JOptionPane.showInputDialog(null, "please enter your name", "Player " + i, JOptionPane.PLAIN_MESSAGE);
+            balance = Double.parseDouble(JOptionPane.showInputDialog("Please enter the balance"));
+            players[i] = new Player(name, balance);
+
+
+        }
+        for (int i = 0; i < 2; i++) {
+            JOptionPane.showMessageDialog(null, players[i].toString());
+
+
+        }
+    }
+}
